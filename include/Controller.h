78df7a14ef40389d9c8bbb427eeaf02debe90689
Karmesh Yadav <ykarmesh@gmail.com>
2019-02-16 00:28:55 -0500

    Working Code

--- include/Controller.h
+++ include/Controller.h
@@ -1,60 +1,61 @@
 #pragma once
-#include "Player.h"
 #include "Game.h"
+#include "Player.h"
 #include <map>
-#include <vector>
 #include <unordered_map>
-namespace mrsd
-{
-	struct Prediction
-	{
-		float t, x;
-	};
-	class Controller
-	{
-		public:
-			Controller(): p(0){}
-			/*
+#include <vector>
+namespace mrsd {
+struct Prediction {
+    float t, x;
+};
+class Controller {
+public:
+    Controller()
+        : p(0)
+    {
+    }
+    /*
 			 * Deconstructor for the controller class
 			 */
-			~Controller();
-			/*
+    ~Controller();
+    /*
 			 * Control AI based on state of game. "t" time has passed since last update.
 			 */
-			void control(const Game& g, float t);
-			/*
+    void control(const Game& g, float t);
+    /*
 			 * Construct Player and add to game.
 			 */
-			void createPlayer(Game& g);
-		private:
-			Player* p;
-			float currentTime;
-			int *dangerZone;
-			int future_duration = 5;
-			std::list<Explosion> future_explosions;
-			std::unordered_map<float, float> exp_pred;
-			float explosion_thres_time = 2.f;
-			float explosion_thres_size = 1.f;
-			/*
+    void createPlayer(Game& g);
+
+private:
+    Player* p;
+    float currentTime;
+    int* dangerZone;
+    int future_duration = 5;
+    std::list<Explosion> future_explosions;
+    std::unordered_map<float, float> exp_pred;
+    float explosion_thres_time = 5.f;
+    float explosion_thres_size = 0.f;
+    /*
 			 * Track projectile and determine where and when it will hit ground.
 			 */
-			Prediction trackProjectile(const Projectile& p, const Game& g, float t);
+    Prediction trackProjectile(const Projectile& p, const Game& g, float t);
 
-			/*
+    /*
 			 * Determine safe spots at time t based on state of all observed projectiles.
 			 */
-			void determineSafeSpots(const Game& g, float t);
+    void determineSafeSpots(const Game& g, float t);
 
-			/*
+    /*
 			 * Pick safe spot from determined safe spots.
 			 */
-			int pickSafeSpot(const Game& g, float t);
+    int pickSafeSpot(const Game& g, float t);
 
-			/*
+    /*
 			 * Function for starting explosion
 			 */
-			void explode(const Game& g, float x);
+    void explode(const Game& g, float x);
 
-			bool tickExplosion(const Game& g, Explosion& e, float t);
-	};
+    bool tickExplosion(const Game& g, Explosion& e, float t);
+};
 }
--- src/Controller.cpp
+++ src/Controller.cpp
@@ -1,43 +1,46 @@
 #include "Controller.h"
 #include <cmath>
-#include <iostream>
 #include <float.h>
-namespace mrsd
+#include <iostream>
+namespace mrsd {
+Controller::~Controller()
 {
-	Controller::~Controller() 
-	{
-		delete[] dangerZone;
-	}
+    delete[] dangerZone;
+}
 
-	void Controller::control(const mrsd::Game& g, float t)
-	{	
-		dangerZone = new int[g.getWidth()+1];
-		for(int i = 0; i < g.getWidth()+1; ++i) dangerZone[i] = 0;
-		std::list<Projectile> proj = g.getProjectiles();
-		for(std::list<Projectile>::iterator it = proj.begin(); it != proj.end();it++) {
-			Prediction p = trackProjectile(*it,g,t);
-			if(exp_pred[p.x] != 0)
-				exp_pred[p.x] = p.t;
-		}
-		determineSafeSpots(g,t);
-		currentTime += t;
-		//pickSafeSpot(g,t);
-	}
+void Controller::control(const mrsd::Game& g, float t)
+{
+    if (p != 0) {
+        std::list<Projectile> proj = g.getProjectiles();
+        for (std::list<Projectile>::iterator it = proj.begin(); it != proj.end(); it++) {
+            Prediction pred = trackProjectile(*it, g, t);
+            if (exp_pred[pred.x] == 0) {
+                exp_pred[pred.x] = pred.t;
+            }
+        }
+        currentTime += t;
+        determineSafeSpots(g, t);
+        pickSafeSpot(g, t);
+    } else {
+        currentTime = 0;
+        dangerZone = new int[g.getWidth() + 1];
+        for (int i = 0; i < g.getWidth() + 1; ++i)
+            dangerZone[i] = 0;
+    }
+}
 
-	void Controller::createPlayer(Game& g)
-	{
-		if(p == 0)
-		{
-			p = new Player();
-			p->dead = true;
-		}
-		if(p->dead)
-		{
-			p->dead = false;
-			p->x = g.getWidth()/2;
-			g.newPlayer(p);
-		}
-		/*int timesteps = 10;
+void Controller::createPlayer(Game& g)
+{
+    if (p == 0) {
+        p = new Player();
+        p->dead = true;
+    }
+    if (p->dead) {
+        p->dead = false;
+        p->x = g.getWidth() / 2;
+        g.newPlayer(p);
+    }
+    /*int timesteps = 10;
 		occupancyGrid = new int*[timesteps];
 		for(int i = 0; i < timesteps; i++) {
 			occupancyGrid[i] = new int[g.getWidth()+1];
@@ -45,87 +48,97 @@ namespace mrsd
 				occupancyGrid[i][j] = 0;
 			}
 		}*/
-	}
+}
 
-	Prediction Controller::trackProjectile(const Projectile& p, const Game& g, float t)
-	{
-		Prediction pred;
-		Projectile temp_p = p;
-		float gravity = g.getGravity();
-		float time_elaspsed = 0;
-		while(temp_p.y >= 0) {
-			temp_p.x += temp_p.vx * t;
-			temp_p.y += temp_p.vy * t + .5f * t * t * gravity;
-			temp_p.vy += t * gravity;
-			time_elaspsed = time_elaspsed + t;
-		}
-		pred.t = currentTime + time_elaspsed;
-		pred.x = temp_p.x;
-		return pred;
-	}
+Prediction Controller::trackProjectile(const Projectile& p, const Game& g, float t)
+{
+    Prediction pred;
+    Projectile temp_p = p;
+    float gravity = g.getGravity();
+    float time_elaspsed = 0;
+    while (temp_p.y >= 0) {
+        temp_p.x += temp_p.vx * t;
+        temp_p.y += temp_p.vy * t + .5f * t * t * gravity;
+        temp_p.vy += t * gravity;
+        time_elaspsed = time_elaspsed + t;
+    }
+    pred.t = currentTime + time_elaspsed;
+    pred.x = temp_p.x;
+    return pred;
+}
 
-	void Controller::determineSafeSpots(const Game& g, float t)
-	{
-		for(std::unordered_map<float,float>::iterator it = exp_pred.begin(); it != exp_pred.end();) 
-		{
-			if(it->second < currentTime + explosion_thres_time)
-			{
-				explode(g,it->first);
-			}
-		}
-		/*for(std::list<Explosion>::iterator it = future_explosions.begin(); it != future_explosions.end();)	
-		{
-			if(!tickExplosion(g,*it,t))
-				it = future_explosions.erase(it);
-			else
-				++it;
-		}*/
-	}
+void Controller::determineSafeSpots(const Game& g, float t)
+{
+    for (std::unordered_map<float, float>::iterator it = exp_pred.begin(); it != exp_pred.end(); it++) {
+        if (it->second < currentTime + explosion_thres_time) {
+            explode(g, it->first);
+            it->second = FLT_MAX;
+        }
+    }
+    for (std::list<Explosion>::iterator it = future_explosions.begin(); it != future_explosions.end();) {
+        if (!tickExplosion(g, *it, t))
+            it = future_explosions.erase(it);
+        else
+            ++it;
+    }
+}
 
-	void Controller::explode(const Game& g, float x)
-	{
-		const float expSize = g.explosionSize;
-		future_explosions.push_back(Explosion());
-		Explosion& exp = future_explosions.back();
-		exp.time = -explosion_thres_time;
-		exp.x = x;
-		exp.y = 0;
-		for(int i = std::floor(exp.x - expSize);
-				i <= std::ceil(exp.x + expSize);
-				++i)
-		{
-			if( i >= 0 && i <= g.getWidth())
-				dangerZone[i]++;
-		}
-	}
+void Controller::explode(const Game& g, float x)
+{
+    const float expSize = g.explosionSize;
+    future_explosions.push_back(Explosion());
+    Explosion& exp = future_explosions.back();
+    exp.time = -explosion_thres_time;
+    exp.x = x;
+    exp.y = 0;
+    for (int i = std::floor(exp.x - expSize - explosion_thres_size);
+         i <= std::ceil(exp.x + expSize + explosion_thres_size);
+         ++i) {
+        if (i >= 0 && i <= g.getWidth())
+            dangerZone[i]++;
+    }
+}
 
-	bool Controller::tickExplosion(const Game& g, Explosion& e, float t)
-	{
-		e.time += t;
-		if(e.time > g.explosionTime)
-		{
-			for(int i = std::floor(e.x - g.explosionSize - explosion_thres_size);
-					i <= std::ceil(e.x + g.explosionSize + explosion_thres_size);
-					++i)
-			{
-				if( i >= 0 && i <= g.getWidth())
-					dangerZone[i]--;
-			}
-			return false;
-		}
-		return true;
-	}
+bool Controller::tickExplosion(const Game& g, Explosion& e, float t)
+{
+    e.time += t;
+    if (e.time > g.explosionTime) {
+        for (int i = std::floor(e.x - g.explosionSize - explosion_thres_size);
+             i <= std::ceil(e.x + g.explosionSize + explosion_thres_size);
+             ++i) {
+            if (i >= 0 && i <= g.getWidth())
+                dangerZone[i]--;
+        }
+        return false;
+    }
+    return true;
+}
 
-	int Controller::pickSafeSpot(const Game& g, float t)
-	{
-		int min_index = std::floor(p->x);
-		//int max_index = std::ceil(p->x);
-		while(dangerZone[min_index] == 1) 
+int Controller::pickSafeSpot(const Game& g, float t)
+{
+    int min_index = std::floor(p->x);
+    //int max_index = std::ceil(p->x);
+    /*while(dangerZone[min_index] == 1) 
 		{
 			p->x += g.playerSpeed * t;
 			min_index = std::floor(p->x);
-		} 
-
-		return 0;
-	}
+		} */
+    for (int i = 0; i < g.getWidth() + 1; i++) {
+        std::cout << dangerZone[i] << " ";
+    }
+    std::cout << '\n';
+    if (dangerZone[min_index] == 1) {
+        int i = min_index, j = min_index;
+        while (dangerZone[i] != 0)
+            i = i + 3;
+        while (dangerZone[j] != 0)
+            j = j - 3;
+        if (min_index - j < i - min_index)
+            p->x -= g.playerSpeed;
+        else
+            p->x += g.playerSpeed;
+        std::cout << p->x << "\n \n \n";
+    }
+    return 0;
+}
 }
